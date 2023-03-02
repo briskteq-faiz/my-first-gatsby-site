7abacb1e3233ffce50dd725b0bd5b8df8b47f153
Faiz Khan <faiz.khan@briskteq.com>
2023-03-02 12:06:27 +0530

    Added a Gatsby image Plugin

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,18 @@
-import * as React from 'react'
-import Layout from '../components/layout'
-
+import * as React from "react";
+import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        src="../images/icon.png"
+        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+      />
     </Layout>
-  )
-}
+  );
+};
 
-export const Head = () => <title>Home Page</title>
+export const Head = () => <title>Home Page</title>;
 
-export default IndexPage
+export default IndexPage;
